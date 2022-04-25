e1e61f0628742cdb24f56402e86dfd1c88d8f90d
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-04-24 22:29:24 -0300

    Ejercicios Realizados

--- Ejercicios/ejercicio2.ts
+++ Ejercicios/ejercicio2.ts
@@ -1,1 +1,12 @@
-console.log("Ejercicio 2:");
+console.log("Avanzar");
+console.log("Llenar 1");
+console.log("Avanzar");
+console.log("Avanzar");
+console.log("Avanzar");
+console.log("Llenar 1");
+console.log("Avanzar");
+console.log("Quitar 1");
+console.log("Avanzar");
+console.log("Quitar 1");
+console.log("Avanzar");
+console.log("FIN Ejercicio 2:");
--- Ejercicios/ejercicio3.ts
+++ Ejercicios/ejercicio3.ts
@@ -1,1 +1,9 @@
-console.log("Ejercicio 3:");
+console.log("Girar Izquierda 90°");
+console.log("Avanzar");
+console.log("Girar Derecha 90°");
+console.log("Avanzar");
+console.log("Avanzar");
+console.log("Girar Derecha 90°");
+console.log("Avanzar");
+console.log("Quitar 1");
+console.log("FIN Ejercicio 3:");
